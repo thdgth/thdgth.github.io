a47b72dc03e7c39074e04b9effb6c6bb66cde9e9
thdgth <thdgth@163.com>
2020-05-26 18:33:58 +0800

    Update message.js

--- live2d/js/message.js
+++ live2d/js/message.js
@@ -1,5 +1,11 @@
 var in_rand = 1;
 var in_order = 2;
+var numid; 
+var clothid;
+var modelnums;
+var loadImg = [];
+const can = document.getElementById('loading');
+const van = can.getContext('2d');
 
 function renderTip(template, context) {
     var tokenReg = /(\\)?\{([^\{\}\\]+)(\\)?\}/g;
@@ -174,13 +180,6 @@ function hideMessage(timeout){
     $('.message').delay(timeout).fadeTo(200, 0);
 }
 
-var numid; 
-var clothid;
-var modelnums;
-var loadImg = [];
-const can = document.getElementById('loading');
-const van = can.getContext('2d');
-
 function loadpngs(url){
     var request = new XMLHttpRequest();
     request.open("get", url);
@@ -268,40 +267,31 @@ function drawCircle() {
 }
 
 function initModels(){
-    if(sessionStorage.getItem("isReload")){
-        console.log("不是第一次进该页面");
-        var list = document.getElementById("landlord");
-        list.removeChild(document.getElementById("loading"));
-        loadRandModel();
-    }else{
-        console.log("是首次加载页面，开始缓存live2d");
-        sessionStorage.setItem("isReload", true);
-        var url = "/live2d/model/model_list.json";
-        var request = new XMLHttpRequest();
-        request.open("get", url);
-        request.send(null);
-        request.onload = function() {
-            if (request.status == 200) {
-                var json = JSON.parse(request.responseText);
-                var models = json.models;
-                modelnums = models.length;
-                for(var i = 0; i < models.length; i++){
-                    if(Array.isArray(models[i])){
-                        modelnums += models[i].length - 1;
-                    }
+    var url = "/live2d/model/model_list.json";
+    var request = new XMLHttpRequest();
+    request.open("get", url);
+    request.send(null);
+    request.onload = function() {
+        if (request.status == 200) {
+            var json = JSON.parse(request.responseText);
+            var models = json.models;
+            modelnums = models.length;
+            for(var i = 0; i < models.length; i++){
+                if(Array.isArray(models[i])){
+                    modelnums += models[i].length - 1;
                 }
-                for(var i = 0; i < models.length; i++){
-                    if(Array.isArray(models[i])){
-                        for(var j = 0; j < models[i].length; j++){
-                            var ModelURL = models[i][j];
-                            console.log('live2d', '加载模型' + i + '.' + j);
-                            loadpngs("/live2d/model/" + ModelURL + "/index.json");
-                        }
-                    }else{
-                        var ModelURL = models[i];
-                        console.log('live2d', '加载模型' + i);
+            }
+            for(var i = 0; i < models.length; i++){
+                if(Array.isArray(models[i])){
+                    for(var j = 0; j < models[i].length; j++){
+                        var ModelURL = models[i][j];
+                        console.log('live2d', '加载模型' + i + '.' + j);
                         loadpngs("/live2d/model/" + ModelURL + "/index.json");
                     }
+                }else{
+                    var ModelURL = models[i];
+                    console.log('live2d', '加载模型' + i);
+                    loadpngs("/live2d/model/" + ModelURL + "/index.json");
                 }
             }
         }
@@ -371,4 +361,81 @@ function loadRandModelClothes(model = in_rand){
             }
         }
     }
+}
+
+window.onload = function () {
+    C = Math.cos; // cache Math objects
+    S = Math.sin;
+    U = 0;
+    w = window;
+    j = document;
+    d = j.getElementById("c");
+    c = d.getContext("2d");
+    W = d.width = w.innerWidth;
+    H = d.height = w.innerHeight;
+    c.fillRect(0, 0, W, H); // resize <canvas> and draw black rect (default)
+    c.globalCompositeOperation = "lighter"; // switch to additive color application
+    c.lineWidth = 0.2;
+    c.lineCap = "round";
+    var bool = 0, 
+        t = 0; // theta
+    d.onmousemove = function (e) {
+        if(window.T) {
+            if(D==9) { D=Math.random()*15; f(1); }
+            clearTimeout(T);
+        }
+        X = e.pageX; // grab mouse pixel coords
+        Y = e.pageY;
+        a=0; // previous coord.x
+        b=0; // previous coord.y 
+        A = X, // original coord.x
+        B = Y; // original coord.y
+        R=(e.pageX/W * 999>>0)/999;
+        r=(e.pageY/H * 999>>0)/999;
+        U=e.pageX/H * 360 >>0;
+        D=9;
+        g = 360 * Math.PI / 180;
+        T = setInterval(f = function (e) { // start looping spectrum
+            c.save();
+            c.globalCompositeOperation = "source-over"; // switch to additive color application
+            if(e!=1) {
+                c.fillStyle = "rgba(0,0,0,0.02)";
+                c.fillRect(0, 0, W, H); // resize <canvas> and draw black rect (default)
+            }
+            c.restore();
+            i = 25; while(i --) {
+                c.beginPath();
+                if(D > 450 || bool) { // decrease diameter
+                    if(!bool) { // has hit maximum
+                        bool = 1;
+                    }
+                    if(D < 0.1) { // has hit minimum
+                        bool = 0;
+                    }
+                    t -= g; // decrease theta
+                    D -= 0.1; // decrease size
+                }
+                if(!bool) {
+                    t += g; // increase theta
+                    D += 0.1; // increase size
+                }
+                q = (R / r - 1) * t; // create hypotrochoid from current mouse position, and setup variables (see: http://en.wikipedia.org/wiki/Hypotrochoid)
+                x = (R - r) * C(t) + D * C(q) + (A + (X - A) * (i / 25)) + (r - R); // center on xy coords
+                y = (R - r) * S(t) - D * S(q) + (B + (Y - B) * (i / 25));
+                if (a) { // draw once two points are set
+                    c.moveTo(a, b);
+                    c.lineTo(x, y)
+                }
+                c.strokeStyle = "hsla(" + (U % 360) + ",100%,50%,0.75)"; // draw rainbow hypotrochoid
+                c.stroke();
+                a = x; // set previous coord.x
+                b = y; // set previous coord.y
+            }
+            U -= 0.5; // increment hue
+            A = X; // set original coord.x
+            B = Y; // set original coord.y
+        }, 16);
+    }
+    j.onkeydown = function(e) { a=b=0; R += 0.05 }
+    d.onmousemove({pageX:300, pageY:290})
 }
