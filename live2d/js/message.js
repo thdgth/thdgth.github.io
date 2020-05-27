382eca4717bad601f79b3260567dfbf48be8ba3b
thdgth <thdgth@163.com>
2020-05-27 10:33:22 +0800

    Update message.js

--- live2d/js/message.js
+++ live2d/js/message.js
@@ -79,7 +79,7 @@ $('.tool .fui-photo').click(function (){
                             var text = tips.text;
                             if(Array.isArray(tips.text)) text = tips.text[Math.floor(Math.random() * tips.text.length + 1)-1];
                             text = text.renderTip({text: $(this).text()});
-                            showMessage(text, 3000);
+                            showMessage("你"+text, 3000);
                         });
                     }else{
                         $(tips.selector).mouseover(function (){
