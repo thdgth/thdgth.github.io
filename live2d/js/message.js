26764602c6b0376021b390e4db8a327a90509c49
thdgth <thdgth@163.com>
2020-06-01 11:23:56 +0800

    Update by GTH

--- live2d/js/message.js
+++ live2d/js/message.js
@@ -81,12 +81,21 @@ $('.tool .fui-photo').click(function (){
                 }
             });
             $.each(result.click, function (index, tips){
-                $(tips.selector).click(function (){
-                    var text = tips.text;
-                    if(Array.isArray(tips.text)) text = tips.text[Math.floor(Math.random() * tips.text.length + 1)-1];
-                    text = text.renderTip({text: $(this).text()});
-                    showMessage(text, 3000);
-                });
+                if($(tips.selector).is('.class')){
+                    $(tips.selector).click(function (){
+                        var text = tips.text;
+                        if(Array.isArray(tips.text)) text = tips.text[Math.floor(Math.random() * tips.text.length + 1)-1];
+                        text = text.renderTip({text: $(this).text()});
+                        showMessage(text, 3000);
+                    });
+                }else{
+                    $(document).on('click', tips.selector, function(e){
+                        var text = tips.text;
+                        if(Array.isArray(tips.text)) text = tips.text[Math.floor(Math.random() * tips.text.length + 1)-1];
+                        text = text.renderTip({text: $(this).text()});
+                        showMessage(text, 3000);
+                    });
+                }
             });
         }
     });
